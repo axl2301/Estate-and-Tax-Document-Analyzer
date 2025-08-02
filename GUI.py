fe6d09e47a1afc16ea752eb7e20e343d3a3a1bf2
axl2301 <126207147+axl2301@users.noreply.github.com>
2025-08-01 20:23:05 -0600

    feat: created repo structure

---
+++ GUI.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(
+    api_key=api_key
+    )
+
+response = client.responses.create(
+  model="gpt-4o-mini",
+  input="write a haiku about ai",
+  store=True,
+)
+
+print(response.output_text)
